e47a49eceb727f6be5024b24d2a1c90d2b069514
daylanKifky <brunolaurencich@gmail.com>
2018-02-05 13:08:29 +0100

    using local-world conversions

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # [WIP] Converting the MCU mocap database to Blender
 
 The idea is been able to create an arbitriary armature in blender and then run this script to convert all the animations of the database to this new base pose
+
+![Armature simplyfing](img/Screenshot_2018-02-05_11-50-57.png)
--- test_simplify.py
+++ test_simplify.py
@@ -2,11 +2,62 @@ import bpy, bmesh
 from mathutils import *
 
 D = bpy.data
+C = bpy.context
 
 def clean_empties():
 	for ob in D.scenes['Scene'].objects:
 			if ob.type == "EMPTY" or ob.type == "MESH":
 				D.scenes['Scene'].objects.unlink(ob)
+
+
+def bone_vec_to_world(vec):
+	try:
+		if vec.owner.id_data.type != "ARMATURE":
+			raise Exception("The vector owner must be an Armature")
+		
+	except Exception as e:
+		print("Can't convert bone vector to World space, probably the vector didn't belong to a bone, see error below")
+		raise e
+
+	ob = vec.owner.id_data
+	bone = vec.owner.data
+
+	m_vec  = Matrix()
+	m_vec = m_vec.Translation(vec)
+	m_vec = ob.convert_space(bone, m_vec, "POSE",  "WORLD")
+
+	return m_vec.to_translation()
+	#end bone_vec_to_world
+	#
+
+def bone_rot_to_world(rot):
+	try:
+		if rot.owner.id_data.type != "ARMATURE":
+			raise Exception("The rotation owner must be an Armature")
+		
+	except Exception as e:
+		print("Can't convert bone rotation to World space, probably the rotation didn't belong to a bone, see error below")
+		raise e
+
+	ob = rot.owner.id_data
+	bone = rot.owner.data
+
+	m_rot  = rot.to_matrix()
+	# m_rot.Translation(rot)
+	m_rot = ob.convert_space(bone, m_rot.to_4x4(), "LOCAL",  "WORLD")
+
+	return m_rot.to_quaternion()
+	#end bone_vec_to_world
+
+
+def enter_edit_mode(ob):
+	bpy.ops.object.mode_set(mode = 'OBJECT')
+	bpy.ops.object.select_all(action = "DESELECT")
+	ob.select = True
+	bpy.context.scene.objects.active = ob
+	bpy.ops.object.mode_set(mode = 'EDIT')
+
+
 
 # Assign a collection
 class BoneList(bpy.types.PropertyGroup):
@@ -32,26 +83,39 @@ relations = {\
 
 magnitudes = {}
 
-bpy.ops.object.mode_set(mode = 'OBJECT')
-bpy.ops.object.select_all(action = "DESELECT")
-target.select = True
-bpy.context.scene.objects.active = target
-bpy.ops.object.mode_set(mode = 'EDIT')
+# clean_empties()
 
-for bone in relations:
+
+for rel in sorted(relations.items()):
+
+	enter_edit_mode(target)
+	bone = rel[0]
+
 	print("POSITIONING: {}".format(bone))
 
-	first = relations[bone][0]
-	
-	second = relations[bone][1]
+	first = rel[1][0]
 
-	first = source.data.bones[first]
-	second = source.data.bones[second]
+	second = rel[1][1]
+
+	first = source.pose.bones[first]
+	second = source.pose.bones[second]
 
 	# Position
-	a = first.head_local
-	b = second.tail_local
+	a = bone_vec_to_world(first.head)
+	b = bone_vec_to_world(second.tail)
+
+	#Get the direction in LOCAL Space
 	direction = b - a
+
+	#CREATE SOME EMPTIES
+	# bpy.ops.object.mode_set(mode = 'OBJECT')
+	# bpy.ops.object.empty_add(location = a, radius = 0.2)
+	# C.object.name = "%s_head" % first.head.owner.data.name 
+	# C.object.show_name = True
+	# bpy.ops.object.empty_add(location = b, radius = 0.2)
+	# C.object.name = "%s_tail" % second.tail.owner.data.name
+	# C.object.show_name = True 
+	# enter_edit_mode(target)
 
 	the_bone = target.data.edit_bones[bone]
 	magnitude = the_bone.vector.magnitude
@@ -59,7 +123,12 @@ for bone in relations:
 	the_bone.tail = the_bone.head + direction.normalized() * magnitude
 
 	# Rotation
-	quat = first.matrix_local.to_quaternion().slerp(second.matrix_local.to_quaternion(),0.5)
+	f_rot = bone_rot_to_world(first.rotation_quaternion)
+	s_rot = bone_rot_to_world(second.rotation_quaternion)
+
+	quat = f_rot.slerp(s_rot, 0.5)
+
+	#Extract twist
 	axis = Vector(quat[1:])
 	axis.normalize()
 
